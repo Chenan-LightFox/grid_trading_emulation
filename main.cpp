b8408aab03c24544823925fd075dc3bdc98711c3
ParseeLemonade <chenan050315@163.com>
2025-03-07 14:40:27 +0800

    A frontpage in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,37 @@
 #include <iostream>
+#include <Windows.h>
+
+void welcome_page()
+{
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    std::cout << "\n\n\n\n\t\t\tWelcome to Grid Trading Emulator!";
+    std::cout << "\n\t\t\t[1] Continue";
+    SetConsoleTextAttribute(hConsole, FOREGROUND_RED);
+    std::cout << "\t\t[2]Exit\n";
+    SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
+}
+
+void get_start()
+{
+    int operation = 0;
+    std::cin >> operation;
+    switch (operation)
+    {
+    case 1:
+        break;
+    case 2:
+        break;
+    default:
+        std::cout << "Invalid input! Please retry.";
+        get_start();
+        break;
+    }
+}
 
 int main()
 {
-    
+    welcome_page();
+    get_start();
+
     return 0;
 }
