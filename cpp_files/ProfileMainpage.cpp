9cfbb070cde184783948ab18839415481ad1d556
ParseeLemonade <chenan050315@163.com>
2025-03-09 22:51:32 +0800

    Add ProfileMainpage

--- cpp_files/ProfileMainpage.cpp
+++ cpp_files/ProfileMainpage.cpp
@@ -7,5 +7,15 @@ ProfileMainpage::~ProfileMainpage() {}
 
 void ProfileMainpage::show_mainpage()
 {
-    std::cout << "ProfileMainpage is under construction.";
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    system("cls");
+    std::cout << "\n\n\n\n\t\t\tTrading Profile";
+    std::cout << "\n\t\t\t---------------";
+    std::cout << "\n\n\t\t\t[1] Set your trading profile.";
+    std::cout << "\n\n\t\t\t[2] View your trading profile.";
+    SetConsoleTextAttribute(hConsole, FOREGROUND_RED);
+    std::cout << "\n\n\t\t\t[3] Delete your trading profile.";
+    SetConsoleTextAttribute(hConsole, FOREGROUND_BLUE);
+    std::cout << "\n\n\t\t\t[4] Back to main menu.\n";
+    SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
 }
