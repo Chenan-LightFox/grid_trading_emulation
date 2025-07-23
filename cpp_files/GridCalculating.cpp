e1a4c1cf7b5d79447a37b33a421c6b4dd674b583
ParseeLemonade <chenan050315@163.com>
2025-07-23 11:56:33 +0800

    Use UTF-8 with BOM to save files

--- cpp_files/GridCalculating.cpp
+++ cpp_files/GridCalculating.cpp
@@ -1,18 +1,18 @@
-#include <iostream>
+﻿#include <iostream>
 #include "../head_files/GridCalculating.h"
 #include "../head_files/PrintLine.h"
 
-// �����ʼ�������
+// 输入初始网格参数
 void Grid::grid_params_input() {
-	print_line("\t\t�����������");
-	print_line("\t\t\t��׽������С\t��", cyan);
+	print_line("\t\t请输入参数：");
+	print_line("\t\t\t捕捉波动大小\t：", cyan);
 	std::cin >> gridSize;
-	print_line("\t\t\tһ�е�������\t��", cyan);
+	print_line("\t\t\t一行的收益率\t：", cyan);
 	std::cin >> profitRate;
-	print_line("\t\t\t�������봥����\t��", cyan);
+	print_line("\t\t\t首行买入触发价\t：", cyan);
 	std::cin >> firstBuyInPrice;
-	print_line("\t\t\tģ������\t\t��", cyan);
+	print_line("\t\t\t模型行数\t\t：", cyan);
 	std::cin >> gridLine;
-	print_line("\t\t\t������\t\t��", cyan);
+	print_line("\t\t\t买入金额\t\t：", cyan);
 	std::cin >> buyInAmounts;
 }
--- cpp_files/main.cpp
+++ cpp_files/main.cpp
@@ -1,15 +1,15 @@
-#include <iostream>
-#include "PrintLine.h"
+﻿#include <iostream>
+#include "../head_files/PrintLine.h"
 using namespace std;
 
 void MainMenu()
 {
-    cout << "\n\n\n\n\t\t\t��ӭʹ�ù�Ʊ�������֣�";
-    cout << "\n\t\t\t------------------------------";
-    print_line("\n\n\t\t\t[1] ��ý��׽���", green);
-    print_line("\n\t\t\t[2] ���ø�����Ϣ", white);
-    print_line("\n\t\t\t[3] �˳�", red);
-    cout << "\n\t\t\t------------------------------\n";
+    print_line("\n\n\n\n\t\t\t欢迎使用网格交易助手！"
+        "\n\t\t\t------------------------------");
+    print_line("\n\n\t\t\t[1] 获取交易策略", green);
+    print_line("\n\t\t\t[2] 设置交易参数");
+    print_line("\n\t\t\t[3] 退出", red);
+    print_line("\n\t\t\t------------------------------\n");
 }
 
 int main()
