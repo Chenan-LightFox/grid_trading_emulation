952a1fec2f6985ee7530a957a0dcefeecbc809db
145a7 <901444805@qq.com>
2025-08-22 17:27:51 +0800

    买入卖出补充

--- cpp_files/main.cpp
+++ cpp_files/main.cpp
@@ -1,9 +1,11 @@
 ﻿#include <iostream>
+#include <cstdlib>
 #include "../head_files/Menus.h"
 
 int main()
 {
-    //自动创建文件夹
+    system("chcp 65001");
+    //自动创建文件夹 
     if (system("cd GTE_Data") == 1) { // 检查GTE_Data文件夹是否存在
         system("cls");
         system("mkdir GTE_Data"); // 创建GTE_Data文件夹
